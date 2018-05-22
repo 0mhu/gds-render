a261f61c5c047e2a233a54d2307524bd725cb213
Mario Hüttel <mario.huettel@gmx.net>
2018-05-22 16:18:28 +0200

    fix commit

---
+++ gds-parser/gds-parser.h
@@ -0,0 +1,29 @@
+/*
+ * GDSII-Converter
+ * Copyright (C) 2018  Mario Hüttel <mario.huettel@gmx.net>
+ *
+ * This file is part of GDSII-Converter.
+ *
+ * GDSII-Converter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * GDSII-Converter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GDSII-Converter.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GDSPARSE_H__
+#define __GDSPARSE_H__
+
+#include <glib.h>
+#include "gds-types.h"
+
+int parse_gds_from_file(const char *filename, GList **library_array);
+int clear_lib_list(GList **library_list);
+
+#endif /* __GDSPARSE_H__ */
---
+++ latex-output/latex-output.h
@@ -0,0 +1,29 @@
+/*
+ * GDSII-Converter
+ * Copyright (C) 2018  Mario Hüttel <mario.huettel@gmx.net>
+ *
+ * This file is part of GDSII-Converter.
+ *
+ * GDSII-Converter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * GDSII-Converter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GDSII-Converter.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LATEX_OUTPUT_H__
+#define __LATEX_OUTPUT_H__
+
+#include "../gds-parser/gds-types.h"
+#include <glib.h>
+#include <stdio.h>
+
+void render_cell_to_code(struct gds_cell *cell, GList *layer_infos, FILE *tex_file);
+
+#endif /* __LATEX_OUTPUT_H__ */
