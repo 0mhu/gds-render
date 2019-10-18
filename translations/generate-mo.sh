7753e420780ba9db74a0134521d25bbd56ec294c
Mario Hüttel <mario.huettel@gmx.net>
2019-10-18 22:51:31 +0200

    Add translation output to Cmake

---
+++ translations/generate-mo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+cd "$DIR"
+
+if [ -z $1 ]; then
+	echo "Must supply an output name"
+	exit -2
+fi
+
+for langdir in `find ./pot/po -mindepth 1 -maxdepth 1 -type d`; do
+	lang=`basename "$langdir"`
+	dest="$1/locale/$lang/LC_MESSAGES"
+	mkdir -p "$dest"
+	pofiles=`find "$langdir" -name "*.po"`
+	msgfmt --output-file="$dest/gds-render.mo" "$pofiles"
+done
