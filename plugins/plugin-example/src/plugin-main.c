392d7e1b3cbe248ca23c61da1f65a1d8af3b085a
Mario Hüttel <mario.huettel@gmx.net>
2019-11-17 14:09:53 +0100

    plugins: Add license header and doxygen file

--- plugins/plugin-example/src/plugin-main.c
+++ plugins/plugin-example/src/plugin-main.c
@@ -1,3 +1,31 @@
+/*
+ * GDSII-Converter example plugin
+ * Copyright (C) 2019  Mario Hüttel <mario.huettel@gmx.net>
+ *
+ * This file is part of GDSII-Converter.
+ *
+ * GDSII-Converter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * GDSII-Converter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GDSII-Converter.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @defgroup example-plugin Example Plugin for External Renderer
+ * @ingroup plugins
+ * This is a template / example for an external renderer plugin
+ * @addtogroup example-plugin
+ * @{
+ */
+
+
 #include <stdio.h>
 #include <glib.h>
 #include <gds-render/gds-utils/gds-types.h>
@@ -17,3 +45,5 @@ int FUNC_DECL(EXTERNAL_LIBRARY_INIT_FUNCTION)(const char *params, const char *ve
 	printf("Init with params: %s\ngds-render version: %s\n", params, version);
 	return 0;
 }
+
+/** @} */
